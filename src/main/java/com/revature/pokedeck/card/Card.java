d9fb17b7d237ca15f708d7db2cb8976d82c1c79f
isaac harris <isaachrr4@gmail.com>
2022-06-28 16:11:03 -0400

    card

--- src/main/java/com/revature/pokedeck/card/Card.java
+++ src/main/java/com/revature/pokedeck/card/Card.java
@@ -7,5 +7,50 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "card")
-public class Card {
+public class Card  implements Comparable <Card>{
+
+    @Id
+    @Column
+    private String Cardid;
+
+    @Column
+    private String Cardurl;
+
+    public Card( String cardid, String cardurl) {
+        Cardid = cardid;
+        Cardurl = cardurl;
+    }
+
+    public Card() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "Cardid='" + Cardid + '\'' +
+                ", Cardurl='" + Cardurl + '\'' +
+                '}';
+    }
+
+    public String getCardid() {
+        return Cardid;
+    }
+
+    public void setCardid(String cardid) {
+        Cardid = cardid;
+    }
+
+    public String getCardurl() {
+        return Cardurl;
+    }
+
+    public void setCardurl(String cardurl) {
+        Cardurl = cardurl;
+    }
+
+    @Override
+    public int compareTo(Card o) {
+        return 0;
+    }
 }

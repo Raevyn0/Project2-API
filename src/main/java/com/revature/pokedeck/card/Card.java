6e20424bb41491eca4a6d7bfc1dc9e782fc1362d
isaac harris <isaachrr4@gmail.com>
2022-06-28 15:21:45 -0400

    card backend structre started

--- src/main/java/com/revature/pokedeck/card/Card.java
+++ src/main/java/com/revature/pokedeck/card/Card.java
@@ -1,4 +1,11 @@
 package com.revature.pokedeck.card;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "card")
 public class Card {
 }
---
+++ src/main/java/com/revature/pokedeck/card/CardController.java
@@ -0,0 +1,4 @@
+package com.revature.pokedeck.card;
+
+public class CardController {
+}

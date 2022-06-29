3437d2bb3ec51984c8a340eb097127fed4df6483
isaac harris <isaachrr4@gmail.com>
2022-06-29 14:19:39 -0400

    card model almost done testing soon

--- src/main/java/com/revature/pokedeck/card/CardController.java
+++ src/main/java/com/revature/pokedeck/card/CardController.java
@@ -1,5 +1,6 @@
 package com.revature.pokedeck.card;
 
+import com.revature.pokedeck.card.dtos.CardResponse;
 import com.revature.pokedeck.user.dtos.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,4 +13,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/cards")
 public class CardController {
+
+    private final CardService cardservice;
+
+    @Autowired
+    public CardController(CardService cardService) {
+        this.cardservice = cardService;
+    }
+
+    @GetMapping(produces = "application/json")
+    public List<CardResponse> getAllCards () {
+        return cardservice.fetchAllCards();
+    }
+
+
 }
--- src/main/java/com/revature/pokedeck/card/CardService.java
+++ src/main/java/com/revature/pokedeck/card/CardService.java
@@ -1,4 +1,19 @@
 package com.revature.pokedeck.card;
 
+import com.revature.pokedeck.card.dtos.CardResponse;
+import com.revature.pokedeck.util.exceptions.ResourceNotFoundException;
+import com.revature.pokedeck.user.dtos.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
 public class CardService {
+    public List<CardResponse> fetchAllCards() {
+        return null;
+    }
 }

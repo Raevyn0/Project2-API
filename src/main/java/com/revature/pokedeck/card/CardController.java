a1b0d97535dd0665c3a4f14835460fe7e93372d9
isaac harris <isaachrr4@gmail.com>
2022-06-30 14:12:34 -0400

    card controller

--- src/main/java/com/revature/pokedeck/card/CardController.java
+++ src/main/java/com/revature/pokedeck/card/CardController.java
@@ -1,7 +1,6 @@
 package com.revature.pokedeck.card;
 
 import com.revature.pokedeck.card.dtos.CardResponse;
-import com.revature.pokedeck.user.dtos.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;

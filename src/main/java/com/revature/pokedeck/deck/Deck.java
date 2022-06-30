2c3e0c25c552f488ac87f4de0791909b4aa1d70c
ZacharyCooremans <77285580+ZacharyCooremans@users.noreply.github.com>
2022-06-30 14:17:45 -0500

    WE CAN FINALLY SEE THE FAVOREITE CARDSSSSS

--- src/main/java/com/revature/pokedeck/deck/Deck.java
+++ src/main/java/com/revature/pokedeck/deck/Deck.java
@@ -2,22 +2,22 @@ package com.revature.pokedeck.deck;
 
 import com.revature.pokedeck.user.User;
 
-import javax.persistence.*;
-import java.util.List;
-
-@Entity
-@Table(name = "decks")
-public class Deck {
-
-    @Id
-    private int id;
-
-    @Column(nullable = false)
-    private String name;
-
-    @JoinColumn(name = "creator_id")
-    private User creator;
-
-    @OneToMany(mappedBy = "deckId")
-    private List<DeckCard> cards;
-}
+//import javax.persistence.*;
+//import java.util.List;
+//
+//@Entity
+//@Table(name = "decks")
+//public class Deck {
+//
+//    @Id
+//    private int id;
+//
+//    @Column(nullable = false)
+//    private String name;
+//
+//    @JoinColumn(name = "creator_id")
+//    private User creator;
+//
+//    @OneToMany(mappedBy = "deckId")
+//    private List<DeckCard> cards;
+//}
